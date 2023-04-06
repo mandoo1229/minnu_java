f448a1e1739a1dd43b81850208754639594654eb
mandoo1229 <skaalsdn35@gmail.com>
2023-04-06 19:04:13 +0900

    book

--- oop/src/book/BookTest.java
+++ oop/src/book/BookTest.java
@@ -5,7 +5,7 @@ public class BookTest {
         Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm');
         System.out.println(ahTeck);  // Author's toString()
 
-        Book dummyBook = new Book("Java for dummy", ahTeck, 19.95, 99);  // Test Book's Constructor
+        Book dummyBook = new Book("Java for dummy", ahTeck, 19.94, 99);  // Test Book's Constructor
         System.out.println(dummyBook);  // Test Book's toString()
 
 // Test Getters and Setters
